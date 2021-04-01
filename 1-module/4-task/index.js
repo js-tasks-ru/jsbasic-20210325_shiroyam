8b8da02469558c13c295c5ce212d007e13a1ea5a
Semyon Zasov <semenmaslina@gmail.com>
2021-04-01 15:25:23 +0300

    task1 task2 task3 task4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  let spam = ['1xbet', 'xxx'];
+  for (item of spam) {
+    if (str.indexOf(item) >= 0) {
+      return true;
+    }
+  }
+  return false;
 }
